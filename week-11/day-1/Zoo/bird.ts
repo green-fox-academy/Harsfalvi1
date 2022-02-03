4901f013628831f35dd704cf7cce75879cb78376
Eva Harsfalvi <harsfalvi.evi@gmail.com>
2022-02-03 13:32:19 +0100

    Add flyable interface to bird in zoo exercise

--- week-11/day-1/Zoo/bird.ts
+++ week-11/day-1/Zoo/bird.ts
@@ -1,12 +1,25 @@
 import { Animal } from "./animals";
 import { Gender } from './gender';
+import { Flyable } from "./../../day-2/Flyable/flyable"
 
-export class Bird extends Animal {
+export class Bird extends Animal implements Flyable {
     constructor (name:string, age?:number, gender?:Gender, color?:string) {
         super (name,age,gender,color)
     }
     
     breed(): string {
         return "by laying eggs."
+    }
+
+    land(): void {
+        console.log("I'm landing.")
+    }
+
+    fly(): void {
+        console.log("I'm flying.")
+    }
+
+    takeOff(): void{
+        console.log("I'm take off.")
     }
 }
