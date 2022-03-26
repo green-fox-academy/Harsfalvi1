7a3189b4ba9878adb60fa9e251fb5bb38b9bc8c3
Harsfalvi1 <harsfalvi.evi@gmail.com>
2022-03-26 14:31:53 +0100

    Add my gallery project

---
+++ week-18-Gallery_project/data.js
@@ -0,0 +1,29 @@
+const images = [
+  {
+    file: "./img1.jpg",
+    title: "Henrik",
+    description: "Henrik ül a karácsonyfa előtt.",
+  },
+  {
+    file: "./img2.jpg",
+    title: "Boldizsár és Henrik",
+    description:
+      "Boldizsár és Henrik a karácsonyfa előtt. Boldizsár mérges. Nem szeret fényképeszkedni.",
+  },
+  {
+    file: "./img3.jpg",
+    title: "Bencével",
+    description: "Henrik, Boldizsár és barátjuk, Bence a karácsonyfa előtt.",
+  },
+  {
+    file: "./img4.jpg",
+    title: "Boldizsár",
+    description:
+      "Boldizsár a karácsonyfa előtt. Megvan a véleménye a karácsonyi fotózás szükségességéről.",
+  },
+  {
+    file: "./img6.jpg",
+    title: "BONUS!",
+    description: "Családunk",
+  },
+];
---
+++ week-18-Gallery_project/index.js
@@ -0,0 +1,80 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const bigImage = document.querySelector(".big-image");
+  bigImage.style.backgroundImage = `url(${images[0].file})`;
+
+  const contentDiv = document.createElement("div");
+
+  const contentTilte = document.createElement("h2");
+  contentTilte.textContent = images[0].title;
+
+  const contentText = document.createElement("p");
+  contentText.textContent = images[0].description;
+
+  contentDiv.setAttribute("class", "content-div");
+
+  contentDiv.appendChild(contentTilte);
+  contentDiv.appendChild(contentText);
+  bigImage.appendChild(contentDiv);
+
+  const thumbnails = document.querySelector(".thumbnails");
+
+  for (let i = 0; i < images.length; i++) {
+    const newDiv = document.createElement("div");
+    newDiv.style.backgroundImage = `url(${images[i].file})`;
+    newDiv.setAttribute("class", "thumbnail-item");
+    thumbnails.appendChild(newDiv);
+  }
+
+  document.querySelectorAll(".thumbnail-item")[0].classList.add("selected");
+
+  document.querySelectorAll(".thumbnail-item").forEach((div, i) => {
+    div.addEventListener("click", () => {
+      bigImage.style.backgroundImage = `url(${images[i].file})`;
+      contentTilte.textContent = images[i].title;
+      contentText.textContent = images[i].description;
+      counter = i;
+    });
+  });
+
+  document.querySelectorAll(".thumbnail-item").forEach((div) => {
+    div.addEventListener("click", (e) => {
+      const selected = document.querySelector(".selected");
+      selected.classList.remove("selected");
+      e.target.classList.add("selected");
+    });
+  });
+
+  const previousButton = document.querySelector(".previous-button");
+  const nextButton = document.querySelector(".next-button");
+  let counter = 0;
+
+  nextButton.addEventListener("click", () => {
+    counter++;
+    if (counter === images.length) {
+      counter = 0;
+    }
+    bigImage.style.backgroundImage = `url(${images[counter].file})`;
+    contentTilte.textContent = images[counter].title;
+    contentText.textContent = images[counter].description;
+    const selected = document.querySelector(".selected");
+    selected.classList.remove("selected");
+    document
+      .querySelectorAll(".thumbnail-item")
+      [counter].classList.add("selected");
+  });
+
+  previousButton.addEventListener("click", () => {
+    counter--;
+    if (counter === -1) {
+      counter = images.length - 1;
+    }
+    bigImage.style.backgroundImage = `url(${images[counter].file})`;
+    contentTilte.textContent = images[counter].title;
+    contentText.textContent = images[counter].description;
+    const selected = document.querySelector(".selected");
+    selected.classList.remove("selected");
+    document
+      .querySelectorAll(".thumbnail-item")
+      [counter].classList.add("selected");
+  });
+});
