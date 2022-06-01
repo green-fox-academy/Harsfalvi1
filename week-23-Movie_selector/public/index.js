d5a99f8184bff968bdf7d5af3f364b96e132687e
Harsfalvi1 <harsfalvi.evi@gmail.com>
2022-06-01 16:29:16 +0200

    Add movieselector

--- week-23-Movie_selector/public/index.js
+++ week-23-Movie_selector/public/index.js
@@ -13,6 +13,37 @@ async function getGenres() {
     return;
   }
 }
+//
+async function getMovies(genre_id) {
+  try {
+    let response;
+    if (!genre_id) {
+      response = await fetch("/api/movies");
+    }
+    response = await fetch(`/api/movies?genre_id=${genre_id}`);
+    console.log(response);
+    if (response.ok) {
+      const data = await response.json();
+      return data.movies;
+    } else {
+      console.error(response.statusText);
+      return;
+    }
+  } catch (error) {
+    console.error(error);
+    return;
+  }
+}
+
+async function createList(genre_id) {
+  let movies = await getMovies(genre_id);
+  let options = '<option value="">Select a genre</option>';
+
+  movies.forEach((movie) => {
+    options += `<option value="${movie.title}">${movie.title}</option>`;
+  });
+  return options;
+}
 
 document.addEventListener("DOMContentLoaded", async () => {
   const genreSelect = document.querySelector("#genre-select");
@@ -21,4 +52,19 @@ document.addEventListener("DOMContentLoaded", async () => {
   genres.forEach((genre) => {
     genreSelect.innerHTML += `<option value="${genre.id}">${genre.genre}</option>`;
   });
+
+  const movieSelect = document.querySelector("#movie-select");
+
+  movieSelect.innerHTML = await createList();
+  const selectedMovie = document.querySelector("#selected-movie");
+
+  genreSelect.onchange = async () => {
+    movieSelect.innerHTML = await createList(genreSelect.value);
+    selectedMovie.textContent = "-";
+  };
+
+  movieSelect.onchange = () => {
+    selectedMovie.textContent = movieSelect.value || "-";
+  };
 });
+
